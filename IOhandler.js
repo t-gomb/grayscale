03cf7c394550b8631aeaf0e9af683c151de3df1f
t-gomb <trentgombas@gmail.com>
2021-11-30 22:22:31 -0800

    added function to unzip a zip file, function to get the path for all the files if they were .png, and function to convert pngs to gray caled images

--- IOhandler.js
+++ IOhandler.js
@@ -1,13 +1,7 @@
-/*
- * Project:
- * File Name: IOhandler.js
- * Description: Collection of functions for files input/output related operations
- *
- * Created Date:
- * Author:
- *
- */
+// Trent Gombas   A01268097
 
+const { rejects } = require("assert");
+const { resolve } = require("path");
 const unzipper = require("unzipper"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
@@ -20,7 +14,18 @@ const unzipper = require("unzipper"),
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = (pathIn, pathOut) => {};
+const unzip = (pathIn, pathOut) => {
+  return new Promise ((resolve, reject) => {
+    if(fs.existsSync(pathOut)){
+      reject('Exists')
+    }else{
+      fs.createReadStream(pathIn)
+      .pipe(unzipper.Extract({ path: pathOut }));
+      resolve('Extraction operation complete')
+    }
+  })
+};
+
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
@@ -28,7 +33,21 @@ const unzip = (pathIn, pathOut) => {};
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
+const readDir = (dir) => {
+  return new Promise ((resolve, reject) => {
+    myArr = []
+    fs.readdir(dir, (err, files) => { 
+      files.forEach(file => {
+        if(path.extname(file) != ".png"){
+        }else{
+          myArr.push(file)
+        }
+      });
+      resolve(myArr)
+  })
+  })
+};
+
 
 /**
  * Description: Read in png file by given pathIn,
@@ -38,7 +57,38 @@ const readDir = (dir) => {};
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {};
+const grayScale = (pathIn, pathOut) => {
+  readDir(pathIn)
+  .then(content => {
+    content.forEach((item) => {
+      fs.createReadStream(pathIn + "\\" + item)
+      
+      .pipe(
+        new PNG({
+          
+        }))
+      .on("parsed", function () {
+        for (var y = 0; y < this.height; y++) {
+          for (var x = 0; x < this.width; x++) {
+            var idx = (this.width * y + x) << 2;
+            let avg = (this.data[idx] + this.data[idx+1] + this.data[idx+2]) / 3
+     
+            // invert color
+            this.data[idx] = avg;
+            this.data[idx + 1] = avg;
+            this.data[idx + 2] = avg;
+     
+            // and reduce opacity
+            this.data[idx + 3] = this.data[idx + 3] >> 1;
+          }
+        }
+        this.pack().pipe(fs.createWriteStream(pathOut + "\\" + item));
+      });
+    })
+    })
+    .catch("error")
+};
+
 
 module.exports = {
   unzip,
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-/*
- * Project:
- * File Name: main.js
- * Description:
- *
- * Created Date:
- * Author:
- *
- */
+// Trent Gombas   A01268097
 
 const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
+
+IOhandler.unzip(zipFilePath, pathUnzipped)
+.then((resolve) => console.log(resolve))
+.catch((err) => console.log(err))
+
+IOhandler.readDir('unzipped')
+.then((resolve) => console.log(resolve))
+.catch((err) => console.log(err))
+
+IOhandler.grayScale(pathUnzipped, pathProcessed)
